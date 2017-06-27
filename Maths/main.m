e9a23306fb114204ede4490a35c9d1b24569349b
escakot <ec@neorennie.com>
2017-06-27 11:50:28 -0400

    Initial Commit

---
+++ Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Errol Cheong on 2017-06-27.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
